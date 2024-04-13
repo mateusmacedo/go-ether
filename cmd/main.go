e3fb01e7f25003b7d9eee25b1934f8f82991851f
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-04-13 18:23:43 -0300

    feat(main): :sparkles: improve worker function to handle jobs continuously

--- cmd/main.go
+++ cmd/main.go
@@ -2,142 +2,136 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"sync"
 	"time"
 )
 
+// Result struct holds the outcome from a job executed by a worker.
 type Result struct {
 	WorkerID int
 	JobID    int
 	Data     interface{}
 }
 
+// Job struct encapsulates an ID and an executable function that simulates workload.
 type Job struct {
 	ID      int
 	Execute func(context.Context) (interface{}, error)
 }
 
+// Worker struct represents a worker with an ID and a waitgroup to synchronize.
 type Worker struct {
 	ID int
 	wg *sync.WaitGroup
 }
 
+// Start is the method to initiate the worker to process jobs from the jobs channel.
 func (w *Worker) Start(ctx context.Context, jobs <-chan Job, results chan<- Result, errs chan<- error) {
-	go func() {
-		for {
-			select {
-			case job, ok := <-jobs:
-				if !ok {
-					fmt.Printf("Worker %d stopped\n", w.ID)
-					return // exit if jobs channel is closed
-				}
-				fmt.Printf("Worker %d started job %d\n", w.ID, job.ID)
-				result, err := job.Execute(ctx)
-				w.wg.Done()
-				if err != nil {
-					errs <- errors.New(fmt.Sprintf("Worker %d job %d error: %s", w.ID, job.ID, err))
-				} else {
-					results <- Result{WorkerID: w.ID, JobID: job.ID, Data: result}
-				}
-			case <-ctx.Done():
-				errs <- errors.New(fmt.Sprintf("Worker %d stopped", w.ID))
-				return
-			}
+	defer w.wg.Done()  // Ensures that the worker's goroutine signals it's done on completion.
+
+	for job := range jobs { // Continuously receive job from jobs channel until it's closed.
+		fmt.Printf("Worker %d started job %d\n", w.ID, job.ID)
+		result, err := job.Execute(ctx)
+
+		if err != nil {
+			errs <- fmt.Errorf("Worker %d job %d error: %s", w.ID, job.ID, err)
+		} else {
+			results <- Result{WorkerID: w.ID, JobID: job.ID, Data: result}
 		}
-	}()
+	}
+
+	fmt.Printf("Worker %d stopped\n", w.ID)
 }
 
+// NewWorker creates a new worker with a given ID and a waitgroup.
 func NewWorker(number int, wg *sync.WaitGroup) Worker {
+	wg.Add(1)  // Increment waitgroup counter on creation of a worker
 	return Worker{
 		ID: number,
 		wg: wg,
 	}
 }
 
+// WorkerPool struct defines the pool of workers including channels for jobs and results.
 type WorkerPool struct {
 	jobs       chan Job
 	results    chan Result
 	errs       chan error
 	maxWorkers int
-	bufferSize int
-	wg         sync.WaitGroup
 }
 
-func NewWorkerPool(maxWorkers, bufferSize int) *WorkerPool {
+// NewWorkerPool initializes a new pool of workers.
+func NewWorkerPool(maxWorkers int) *WorkerPool {
 	return &WorkerPool{
-		jobs:       make(chan Job, bufferSize),
-		results:    make(chan Result, bufferSize),
-		errs:       make(chan error, bufferSize),
+		jobs:       make(chan Job),
+		results:    make(chan Result),
+		errs:       make(chan error),
 		maxWorkers: maxWorkers,
-		bufferSize: bufferSize,
 	}
 }
 
+// Run starts all workers in separate goroutines to process jobs.
 func (wp *WorkerPool) Run(ctx context.Context) {
+	var wg sync.WaitGroup
+
 	for i := 0; i < wp.maxWorkers; i++ {
-		worker := NewWorker(i+1, &wp.wg)
-		go func() {
-			worker.Start(ctx, wp.jobs, wp.results, wp.errs)
-		}()
-		fmt.Printf("Worker %d created\n", worker.ID)
+		worker := NewWorker(i+1, &wg)
+		go worker.Start(ctx, wp.jobs, wp.results, wp.errs)
 	}
+
+	wg.Wait()  // Wait for all workers to finish
+	close(wp.results)  // Close results channel after all jobs are processed
+	close(wp.errs)  // Close errors channel after all jobs are processed
 }
 
+// SubmitJob sends a job to the jobs channel for the workers to pick up.
 func (wp *WorkerPool) SubmitJob(job Job) {
-	wp.wg.Add(1)
 	wp.jobs <- job
 }
 
-func (wp *WorkerPool) Wait() {
-	wp.wg.Wait()
-}
-
+// Close signals to workers by closing the jobs channel.
 func (wp *WorkerPool) Close() {
 	close(wp.jobs)
-	close(wp.results)
-	close(wp.errs)
 }
 
 func main() {
-	workerpool := NewWorkerPool(3, 100) // create dispatcher with 3 workers
+	workerPool := NewWorkerPool(3)  // create pool with 3 workers
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	go func() {
-		for result := range workerpool.results {
+		for result := range workerPool.results {
 			fmt.Printf("Worker %d Job %d result: %v\n", result.WorkerID, result.JobID, result.Data)
 		}
 	}()
 
 	go func() {
-		for err := range workerpool.errs {
+		for err := range workerPool.errs {
 			fmt.Printf("%v\n", err)
 		}
 	}()
 
-	workerpool.Run(ctx)
+	go workerPool.Run(ctx)
 
 	for i := 1; i <= 30; i++ {
-		jobNum := i
 		job := Job{
 			ID: i,
 			Execute: func(jobctx context.Context) (interface{}, error) {
 				select {
 				case <-time.After(1 * time.Second):
-					return fmt.Sprintf("Job completed: %d", jobNum*2), nil
+					return fmt.Sprintf("Job completed: %d", i*2), nil
 				case <-jobctx.Done():
-					return nil, errors.New(fmt.Sprintf("%s", jobctx.Err()))
+					return nil, fmt.Errorf("Job %d canceled: %s", i, jobctx.Err())
 				}
 			},
 		}
-		workerpool.SubmitJob(job)
+
+		workerPool.SubmitJob(job)
 		fmt.Printf("Job %d queued\n", i)
 	}
 
-	workerpool.Wait()
-	workerpool.Close()
+	workerPool.Close()
 	fmt.Println("All jobs processed")
 }
