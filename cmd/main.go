c4a10e32cb8c20cc8b28c12c466a19eb62e002dc
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-04-13 12:53:52 -0300

    feat(main): ✨ introduce Dispatcher to manage job distribution

--- cmd/main.go
+++ cmd/main.go
@@ -7,39 +7,37 @@ import (
 	"time"
 )
 
+// Job represents a job to be executed
 type Job struct {
     ID      int
     Process func(context.Context) error
 }
 
+// Worker represents a worker that can execute jobs
 type Worker struct {
-    ID          int
-    jobChannel  chan Job
-    wg          *sync.WaitGroup
+    ID int
+    wg *sync.WaitGroup
 }
 
-func (w *Worker) Start(ctx context.Context, workerPool chan chan Job) {
+// Start starts the worker
+func (w *Worker) Start(ctx context.Context, jobs <-chan Job, errs chan<- error) {
     go func() {
         for {
             select {
-            case workerPool <- w.jobChannel:
-                select {
-                case job, ok := <-w.jobChannel:
-                    if !ok {
-                        return // exit if channel is closed
-                    }
-                    fmt.Printf("Worker %d started job %d\n", w.ID, job.ID)
-                    err := job.Process(ctx)
-                    if err != nil {
-                        fmt.Printf("Worker %d job %d error: %s\n", w.ID, job.ID, err.Error())
-                    } else {
-                        fmt.Printf("Worker %d finished job %d\n", w.ID, job.ID)
-                    }
-                    w.wg.Done()
-                case <-ctx.Done():
+            case job, ok := <-jobs:
+                if !ok {
                     fmt.Printf("Worker %d stopped\n", w.ID)
-                    return
+                    return // exit if jobs channel is closed
                 }
+                fmt.Printf("Worker %d started job %d\n", w.ID, job.ID)
+                err := job.Process(ctx)
+                if err != nil {
+                    fmt.Printf("Worker %d job %d error: %s\n", w.ID, job.ID, err.Error())
+                    errs <- err
+                } else {
+                    fmt.Printf("Worker %d finished job %d\n", w.ID, job.ID)
+                }
+                w.wg.Done()
             case <-ctx.Done():
                 fmt.Printf("Worker %d stopped\n", w.ID)
                 return
@@ -48,63 +46,71 @@ func (w *Worker) Start(ctx context.Context, workerPool chan chan Job) {
     }()
 }
 
+// NewWorker creates a new worker
 func NewWorker(number int, wg *sync.WaitGroup) Worker {
-    worker := Worker{
-        ID:         number,
-        jobChannel: make(chan Job),
-        wg:         wg,
+    return Worker{
+        ID: number,
+        wg: wg,
     }
-
-    return worker
 }
 
-// Dispatcher - A struct that sends the jobs to available workers
+// Dispatcher sends the jobs to available workers
 type Dispatcher struct {
-    jobQueue   chan Job
-    workerPool chan chan Job
+    jobs       chan Job
     maxWorkers int
     wg         sync.WaitGroup
 }
 
-func NewDispatcher(jobQueue chan Job, maxWorkers int) *Dispatcher {
-    workerPool := make(chan chan Job, maxWorkers)
-    return &Dispatcher{jobQueue: jobQueue, workerPool: workerPool, maxWorkers: maxWorkers}
+// NewDispatcher creates a new dispatcher
+func NewDispatcher(maxWorkers int) *Dispatcher {
+    return &Dispatcher{
+        jobs:       make(chan Job, 100),
+        maxWorkers: maxWorkers,
+    }
 }
 
-func (d *Dispatcher) dispatch(ctx context.Context) {
+// Dispatch starts the dispatcher
+func (d *Dispatcher) Dispatch(ctx context.Context, errs chan<- error) {
     for i := 0; i < d.maxWorkers; i++ {
         worker := NewWorker(i+1, &d.wg)
-        worker.Start(ctx, d.workerPool)
+        worker.Start(ctx, d.jobs, errs)
         fmt.Printf("Worker %d created\n", worker.ID)
     }
 
-    for {
-        select {
-        case job := <-d.jobQueue:
-            jobChannel := <-d.workerPool
-            jobChannel <- job
-        case <-ctx.Done():
-            fmt.Println("Dispatcher stopped")
-            return
-        }
-    }
+    go func() {
+        <-ctx.Done()
+        close(d.jobs)
+        fmt.Println("Dispatcher stopped")
+    }()
 }
 
-func (d *Dispatcher) run(ctx context.Context) {
-    go d.dispatch(ctx)
+// AddJob adds a job to the dispatcher
+func (d *Dispatcher) AddJob(job Job) {
+    d.jobs <- job
+    d.wg.Add(1)
+}
+
+// Wait waits for all jobs to be processed
+func (d *Dispatcher) Wait() {
+    d.wg.Wait()
 }
 
 func main() {
-    jobs := make(chan Job, 100)
-    dispatcher := NewDispatcher(jobs, 3) // create dispatcher with 3 workers
+    dispatcher := NewDispatcher(3) // create dispatcher with 3 workers
 
     ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
     defer cancel()
 
-    dispatcher.run(ctx) // start dispatching jobs with a context
+    errs := make(chan error)
+    go func() {
+        for err := range errs {
+            fmt.Println("Error:", err)
+        }
+    }()
+
+    dispatcher.Dispatch(ctx, errs) // start dispatching jobs with a context
 
     for i := 1; i <= 20; i++ {
-        dispatcher.wg.Add(1)
         job := Job{
             ID: i,
             Process: func(jobctx context.Context) error {
@@ -117,10 +123,11 @@ func main() {
                 }
             },
         }
-        jobs <- job
+        dispatcher.AddJob(job)
         fmt.Printf("Job %d queued\n", i)
     }
 
-    dispatcher.wg.Wait() // wait for all jobs to be processed
+    dispatcher.Wait() // wait for all jobs to be processed
+    close(errs)
     fmt.Println("All jobs processed")
 }
