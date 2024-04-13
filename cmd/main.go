fe57fa645c187240ac595282dbbd4f6d9f656458
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-04-13 20:59:40 -0300

    feat(main): :sparkles: add logging and improve job execution in WorkerPool class

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 	"sync"
 	"time"
 )
@@ -18,31 +20,43 @@ type Job struct {
 	Execute func(context.Context) (interface{}, error)
 }
 
+const (
+	JOB_ID_KEY    = "JobID"
+	WORKER_ID_KEY = "WorkerID"
+    MAX_WORKERS = 3
+    MAX_JOBS = 30
+)
+
+func NoopExecute(ctx context.Context) (interface{}, error) {
+	time.Sleep(1 * time.Second)
+	return fmt.Sprintf("Worker: %d completed Job: %d successfully.", ctx.Value(WORKER_ID_KEY), ctx.Value(JOB_ID_KEY)), nil
+}
+
+func NewJob(id int, execute func(context.Context) (interface{}, error)) Job {
+	return Job{ID: id, Execute: execute}
+}
+
 type Worker struct {
 	ID int
 	wg *sync.WaitGroup
 }
 
 func (w *Worker) Start(ctx context.Context, jobs <-chan Job, results chan<- Result, errs chan<- error) {
-	defer w.wg.Done()
-
 	for job := range jobs {
-		result, err := job.Execute(ctx)
+		result, err := job.Execute(func(c context.Context) context.Context {
+			cjob := context.WithValue(c, JOB_ID_KEY, job.ID)
+			return context.WithValue(cjob, WORKER_ID_KEY, w.ID)
+		}(ctx))
+		w.wg.Done()
 		if err != nil {
-			if err == context.DeadlineExceeded {
-				errs <- fmt.Errorf("Worker %d: Job %d timeout error: %s", w.ID, job.ID, err)
-			} else {
-				errs <- fmt.Errorf("Worker %d: Job %d error: %s", w.ID, job.ID, err)
-			}
+			errs <- fmt.Errorf("Worker %d: Job %d error: %s", w.ID, job.ID, err.Error())
 		} else {
 			results <- Result{WorkerID: w.ID, JobID: job.ID, Data: result}
 		}
 	}
-	fmt.Printf("Worker %d stopped\n", w.ID)
 }
 
 func NewWorker(number int, wg *sync.WaitGroup) Worker {
-	wg.Add(1)
 	return Worker{ID: number, wg: wg}
 }
 
@@ -51,74 +65,72 @@ type WorkerPool struct {
 	results    chan Result
 	errs       chan error
 	maxWorkers int
+	wg         sync.WaitGroup
 }
 
 func NewWorkerPool(maxWorkers int) *WorkerPool {
-	return &WorkerPool{
+	pool := &WorkerPool{
 		jobs:       make(chan Job),
 		results:    make(chan Result),
 		errs:       make(chan error),
 		maxWorkers: maxWorkers,
+		wg:         sync.WaitGroup{},
 	}
+	return pool
 }
 
 func (wp *WorkerPool) Run(ctx context.Context) {
-	var wg sync.WaitGroup
-
 	for i := 0; i < wp.maxWorkers; i++ {
-		worker := NewWorker(i+1, &wg)
+		worker := NewWorker(i+1, &wp.wg)
 		go worker.Start(ctx, wp.jobs, wp.results, wp.errs)
 	}
-
-	wg.Wait()
-	close(wp.results)
-	close(wp.errs)
 }
 
 func (wp *WorkerPool) SubmitJob(job Job) {
+	wp.wg.Add(1)
 	wp.jobs <- job
 }
 
-func (wp *WorkerPool) Close() {
-	close(wp.jobs)
-}
-
 func main() {
+	log.SetOutput(os.Stdout)
 	workerPool := NewWorkerPool(3)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	defer func() {
+		cancel()
+        if r := recover(); r != nil {
+			log.Println("Recovered in main; deferred functions are running")
+		}
+		close(workerPool.jobs)
+		close(workerPool.results)
+		close(workerPool.errs)
+	}()
 
 	go func() {
 		for result := range workerPool.results {
-			fmt.Printf("Result from Worker %d, Job %d: %v\n", result.WorkerID, result.JobID, result.Data)
+			log.Printf("Result from Worker %d, Job %d: %v\n", result.WorkerID, result.JobID, result.Data)
 		}
 	}()
 
 	go func() {
 		for err := range workerPool.errs {
-			fmt.Printf("Error: %v\n", err)
+			log.Printf("Error: %v\n", err)
 		}
 	}()
 
-	go workerPool.Run(ctx)
+    go workerPool.Run(ctx)
 
-	for i := 1; i <= 30; i++ {
-		job := Job{
-			ID: i,
+	for i := 1; i <= MAX_JOBS; i++ {
+		j := i
+		workerPool.SubmitJob(Job{
+			ID: j,
 			Execute: func(jobctx context.Context) (interface{}, error) {
-				select {
-				case <-time.After(800 * time.Millisecond): // Assume some jobs might take longer than the 1 second ideal processing time
-					return fmt.Sprintf("Job %d completed successfully", i), nil
-				case <-jobctx.Done():
-					return nil, jobctx.Err() // Properly handle context cancellation due to timeout
-				}
+				return NoopExecute(jobctx)
 			},
-		}
-
-		workerPool.SubmitJob(job)
+		})
 	}
 
-	workerPool.Close()
-	fmt.Println("All jobs have been processed or timed out.")
+	workerPool.wg.Wait()
+
+	log.Println("All jobs have been completed successfully.")
 }
