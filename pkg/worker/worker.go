4d89b33b8a1ae57acb3156654b92e8e92c70d8b3
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-04-13 23:23:46 -0300

    feat(worker): :sparkles: add Worker struct and methods

---
+++ pkg/worker/worker.go
@@ -0,0 +1,40 @@
+package worker
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	jobpkg "github.com/mateusmacedo/goether/worker/pkg/job"
+)
+
+type Worker struct {
+	ID int
+	wg *sync.WaitGroup
+}
+
+func (w *Worker) Start(ctx context.Context, jobs <-chan jobpkg.Job, results chan<- jobpkg.Result, errs chan<- error) {
+	defer w.wg.Done()
+	for job := range jobs {
+		if job.ID == jobpkg.SHUTDOWN_SIGNAL {
+			return
+		}
+		jobCtx, cancel := context.WithTimeout(ctx, jobpkg.CONTEXT_TIMEOUT)
+		defer cancel()
+
+		jobCtx = context.WithValue(jobCtx, jobpkg.WORKER_ID_KEY, w.ID)
+		jobCtx = context.WithValue(jobCtx, jobpkg.JOB_ID_KEY, job.ID)
+
+		result, err := job.Execute(jobCtx)
+		if err != nil {
+			errs <- fmt.Errorf("worker %d: job %d error: %s", w.ID, job.ID, err)
+			continue
+		}
+		results <- jobpkg.Result{WorkerID: w.ID, JobID: job.ID, Data: result}
+	}
+}
+
+func New(number int, wg *sync.WaitGroup) Worker {
+	wg.Add(1)
+	return Worker{ID: number, wg: wg}
+}
